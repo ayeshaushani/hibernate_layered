30f23c7de31ff0bad1005dfca1000083fd102ab3
ayeshaushani <akisasenari@gmail.com>
2024-09-26 18:10:51 +0530

    commit

---
+++ src/main/java/lk/ijse/Launcher.java
@@ -0,0 +1,25 @@
+package lk.ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+import static javafx.application.Application.launch;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/dashboardform.fxml"))));
+        stage.setTitle("Thoga-kade");
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
